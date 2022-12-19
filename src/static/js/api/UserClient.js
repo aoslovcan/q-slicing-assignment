e5967d92951a3d2de33c4b6ce196a5e99fac1d19
ERICSSON\eoslant <antonija.oslovcan@ericsson.com>
2022-12-19 19:13:31 +0100

    add user client

---
+++ src/static/js/api/UserClient.js
@@ -0,0 +1,19 @@
+import {ApiClient} from "./ApiClient";
+
+export class UserClient extends ApiClient{
+
+    BASE_URl = "https://dummyjson.com";
+    getUserById(id) {
+        const url = `${this.BASE_URl}/users/${id}`;
+        const data = {
+            method: 'GET',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+        };
+
+        return this.fetchApi(url, data);
+    }
+
+}
